5d2ecab60f6465f14461c2860ef118f46e4782b2
Nebado <ruslan.nbd@gmail.com>
2021-03-08 23:03:48 +0200

    Update todo item

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useRef} from 'react'
 
 function TodoForm(props) {
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
     const inputRef = useRef(null);
 
@@ -26,16 +26,34 @@ function TodoForm(props) {
 
     return (
         <form className="todo-form" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Add a task"
-            value={input}
-            name="text"
-            className="todo-input"
-            onChange={handleChange}
-            ref={inputRef}
-          />
-          <button className="todo-button">Add task</button>
+          {props.edit ? (
+              <>
+                <input
+                  type="text"
+                  placeholder="Update your item"
+                  value={input}
+                  name="text"
+                  className="todo-input edit"
+                  onChange={handleChange}
+                  ref={inputRef}
+                  />
+                <button className="todo-button edit">Update</button>
+              </>
+          ) :
+           (
+               <>
+                 <input
+                   type="text"
+                   placeholder="Add a task"
+                   value={input}
+                   name="text"
+                   className="todo-input"
+                   onChange={handleChange}
+                   ref={inputRef}
+                   />
+                 <button className="todo-button">Add task</button>
+               </>
+           )}
         </form>
     )
 }
