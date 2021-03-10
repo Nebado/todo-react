f520f617c7acc17d9b16d5ebfaad51833ba567f1
Nebado <ruslan.nbd@gmail.com>
2021-03-10 22:52:13 +0200

    Add animation of todo items

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import TodoForm from './TodoForm';
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
+import FlipMove from 'react-flip-move';
 
 function Todo({todos, completeTodo, removeTodo, updateTodo}) {
     const [edit, setEdit] = useState({
@@ -21,26 +22,35 @@ function Todo({todos, completeTodo, removeTodo, updateTodo}) {
         return <TodoForm edit={edit} onSubmit={submitUpdate} />;
     }
 
-    return todos.map((todo, index) => (
+    const items = todos.map((todo, index) => (
         <div
           className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
           key={index}
-        >
+          >
           <div key={todo.id} onClick={() => completeTodo(todo.id)}>
             {todo.text}
           </div>
+
           <div className="icons">
             <RiCloseCircleLine
               onClick={() => removeTodo(todo.id)}
               className='delete-icon'
-            />
-            <TiEdit
-              onClick={() => setEdit({id: todo.id, value: todo.text })}
-              className='edit-icon'
-            />
+              />
+              <TiEdit
+                onClick={() => setEdit({id: todo.id, value: todo.text })}
+                className='edit-icon'
+                />
           </div>
         </div>
-    ))
+    ));
+
+    return (
+        <div>
+          <FlipMove duration={300} easing="cubic-bezier(0, 0.7, 0.8, 0.1)">
+            {items}
+          </FlipMove>
+        </div>
+    );
 }
 
 export default Todo;
