c6fa0c5aaf7f82fcc7b271e66df5045dd8b1ea08
Nebado <ruslan.nbd@gmail.com>
2021-03-13 13:57:14 +0200

    Add todo observer

--- src/components/TodoObserver.js
+++ src/components/TodoObserver.js
@@ -3,16 +3,21 @@ import React from 'react';
 class TodoObserver extends React.Component {
     constructor(props) {
         super(props);
-        this.position = 0;
-        this.speed = 5;
-        this.direction = 1;
-        this.stepNum = 1;
+        this.state = {
+            positionX: -515,
+            positionY: -67,
+            left: 10,
+            right: 70,
+            speed: 350,
+            direction: 1,
+            stepNum: 64
+        }
     }
 
     componentDidMount() {
         this.intervalID = setInterval(
             () => this.step(),
-            1000
+            this.state.speed
         );
     }
     componentWillUnmount() {
@@ -20,12 +25,41 @@ class TodoObserver extends React.Component {
     }
 
     step() {
-        // TODO
+        if (this.state.positionX == -3) {
+            this.state.positionX = -515;
+        }
+
+        if (this.state.right === 420) {
+            this.state.direction = 0;
+            this.state.positionY = -196;
+        }
+
+        if (this.state.right === 70) {
+            this.state.direction = 1;
+            this.state.positionY = -67;
+        }
+
+        if (this.state.direction === 1) {
+            this.setState({
+                positionX: this.state.positionX + this.state.stepNum,
+                right: this.state.right+10
+            });
+        } else {
+            this.setState({
+                positionX: this.state.positionX + this.state.stepNum,
+                right: this.state.right-10
+            });
+        }
     }
 
     render() {
+        let style = {
+            backgroundPositionX: this.state.positionX,
+            backgroundPositionY: this.state.positionY,
+            right: this.state.right
+        }
         return (
-            <div className="todo-observer"></div>
+            <div className="todo-observer" style={style}></div>
         );
     }
 }
